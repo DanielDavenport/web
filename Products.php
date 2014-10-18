36e11168c8a77c189aeaac67e3113e031f8baeb2
Kayley Lane <kayley.lane@my.wheaton.edu>
2014-10-17 20:10:04 -0500

    Added a search bar and sort option to the display.
    hecka wasted a ton of time on this

--- Products.php
+++ Products.php
@@ -1,8 +1,4 @@
-<!-- JQUERY -->
-<script src="//code.jquery.com/jquery-1.10.2.js" type="text/javascript"></script
->
-<script src="//code.jquery.com/ui/1.10.4/jquery-ui.js" type="text/javascript"></script>
-<script src="//bgrins.github.io/spectrum/spectrum.js" type="text/javascript"></script>
+
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 
 <?php
@@ -24,9 +20,24 @@ if (mysqli_connect_error())
 
 
 <head>
+   
+<link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
 <title> blonic original character do not steal </title>
 <link rel="shortcut icon" href="http://i1144.photobucket.com/albums/o488/incarce
 rempb/Icons/MonsterGirls/tinycyclops.png">
+
+ <!-- JQUERY -->
+<script src="http://code.jquery.com/jquery-1.10.2.js" type="text/javascript"></script>
+<script src="http://code.jquery.com/ui/1.10.4/jquery-ui.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+
+    $(function () {
+
+    });
+
+
+</script>
 <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?fam
 ily=Oswald">
     <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='st
@@ -93,12 +104,6 @@ background-color: transparent;
 }
 
 
-.section{
-    display:inline-block;
-    width:710px; padding:5px 5px 5px 5px; background:transparent;
-    height:700px; overflow-x:auto; overflow-y:auto;
-}
-
 h1{
     font-size:14px; font-style:italic; font-family:georgia;
     border-radius:50px;
@@ -136,15 +141,122 @@ h3{
 <div id="underneath">
 <a href="/">home</a> | <a href="/ask">ask</a><br><div style="text-transform:lowercase; letter-spacing:1.5px"></div></div>
 
+
 <?php
-//display
+
+$products = array();
+$result = array();
+$searchtext = "";
+$displaytext = "";
+
 $QryStr = "SELECT * FROM Products";
 $Results = mysqli_query($mysqli, $QryStr) or
     die("Failed Query $QryStr: " . mysqli_error($mysqli));
 
-echo "<TABLE ALIGN='center'><TR>";
-$i = 0;
-while ($Object = mysqli_fetch_object($Results)){
+
+while ($Obj = mysqli_fetch_object($Results)){
+        array_push($products, $Obj);
+}
+$result = $products;
+
+if(isset($_GET['search']))
+{
+    $search = $_GET['search'];
+    if($search == "" || ctype_space ($search)){
+        unset($search);
+    }
+    else{
+        $searchtext = $search;
+        search();
+    }
+}
+
+if(isset($_GET['displaymode']))
+{
+    $displaymode = $_GET['displaymode'];
+    $displaytext = $displaymode;
+    fixMode();
+
+} else {
+    $displaymode = "standard";
+}
+
+
+function fixMode(){
+    global $result;
+    global $products;
+    global $displaymode;
+    if (strcmp ( $displaymode , "standard" ) == 0){
+        
+    }else if(strcmp ( $displaymode , "alphabetical" ) == 0){
+        $result = sortArrayofObjectByProperty( $result, "name" );
+    }else if(strcmp ( $displaymode , "reversedalphabetical" ) == 0){
+        $result = array_reverse (sortArrayofObjectByProperty( $result, "name" ));
+    }else if(strcmp ( $displaymode , "price" ) == 0){
+        $result = sortArrayofObjectByProperty( $result, "price" );
+    }else if(strcmp ( $displaymode , "weight" ) == 0){
+        $result = sortArrayofObjectByProperty( $result, "weight" );
+    }else{
+        unset($displaymode);
+    }
+}
+
+
+function search(){
+    global $result;
+    global $products;
+    $result = array();
+    global $search;
+    foreach($products as $Object){
+        //Find all occurances of the search string in our product database. 
+        if(strpos(strtolower ($Object->name), strtolower($search)) !== FALSE){
+            array_push($result, $Object);
+        }
+    }
+}
+
+//Display the search form.
+
+$sortoptions = array(
+    "standard" => "Mysterious",
+    "alphabetical" => "Alphabetical (A-Z)",
+    "reversedalphabetical" => "Alphabetical (Z-A)",
+    "price" => "Price",
+    "weight" => "Weight"
+);
+
+
+echo "<BR><form id = 'searcher' action='' method='get'>
+
+    <fieldset>
+        <label for='searchbar'>Search:</label>
+        <input type='text' name='search' id='searchbar' class ='text ui-widget-content ui-corner-all' maxlength='40' value='" . $searchtext . "'>
+
+        <label for='displaymode'>Sort by:</label>
+        <select name='displaymode' id='displaymode' class='text ui-widget-content ui-corner-all'>";
+
+
+foreach( $sortoptions as $var => $interest ){
+    $isselected = "";
+    if(strcmp ( $var , $displaytext ) == 0){
+        $isselected = "selected = 'selected'";
+     }
+                echo "<option value = '$var' $isselected> $interest </option>";
+        }
+echo "
+        </select>
+
+        <button id='searchbutton'  class ='text ui-widget-content ui-corner-all' >Go!</button>
+    </fieldset>
+
+</form> ";
+
+
+//Display products.
+    echo "<TABLE ALIGN='center'><TR>";
+    $i = 0;
+    global $result;
+    foreach($result as $Object){
         $iden = $Object->pid; 
         echo "<TD><CENTER>";
         echo "<BR><a href='PHPPage.php?productid=" . $iden ."'><img src='" . $Object -> thumbnailUrl . "' width='100px' align='left' class='icon'></a></TD><TD>";
@@ -154,8 +266,72 @@ while ($Object = mysqli_fetch_object($Results)){
         echo "</TD></CENTER>";
         $i++;
         if($i % 4 == 0) echo "</TR><TR>";
+    }
+    echo "</TABLE>";
+
+/**
+* Sort one array of objects by one of the object's property
+*
+* @param mixed $array, the array of objects
+* @param mixed $property, the property to sort with
+* @return mixed, the sorted $array
+* by http://www.mabishu.com/blog/2011/02/03/sort-an-array-of-objects-by-one-of-the-object-property-with-php/
+*/
+function sortArrayofObjectByProperty( $array, $property )
+{
+    $cur = 1;
+    $stack[1]['l'] = 0;
+    $stack[1]['r'] = count($array)-1;
+
+    do
+    {
+        $l = $stack[$cur]['l'];
+        $r = $stack[$cur]['r'];
+        $cur--;
+
+        do
+        {
+            $i = $l;
+            $j = $r;
+            $tmp = $array[(int)( ($l+$r)/2 )];
+
+            // split the array in to parts
+            // first: objects with "smaller" property $property
+            // second: objects with "bigger" property $property
+            do
+            {
+                while( $array[$i]->{$property} < $tmp->{$property} ) $i++;
+                while( $tmp->{$property} < $array[$j]->{$property} ) $j--;
+
+                // Swap elements of two parts if necesary
+                if( $i <= $j)
+                {
+                    $w = $array[$i];
+                    $array[$i] = $array[$j];
+                    $array[$j] = $w;
+
+                    $i++;
+                    $j--;
+                }
+
+            } while ( $i <= $j );
+
+            if( $i < $r ) {
+                $cur++;
+                $stack[$cur]['l'] = $i;
+                $stack[$cur]['r'] = $r;
+            }
+            $r = $j;
+
+        } while ( $l < $r );
+
+    } while ( $cur != 0 );
+
+    return $array;
+
 }
-echo "</TABLE>";
+
+
 ?>
 
 
