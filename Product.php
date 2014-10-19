248a0cff67c74eb9cc5d4faa445c2825c56ac0f1
Kayley Lane <kayley.lane@my.wheaton.edu>
2014-10-19 13:20:12 -0500

    Cleaned up.

--- Product.php
+++ Product.php
@@ -1,36 +1,19 @@
 <!-- JQUERY -->
 <script src="//code.jquery.com/jquery-1.10.2.js" type="text/javascript"></script
->
-<script src="//code.jquery.com/ui/1.10.4/jquery-ui.js" type="text/javascript"></script>
-
+><script src="//code.jquery.com/ui/1.10.4/jquery-ui.js" type="text/javascript"></script>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
 
 <?php
-ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
-error_reporting(-1);
-
-//Redirect to index if useri sn't logged in
-//session_start();
-//if (!(isset($_SESSION['UserName']) || isset($LoginPage)))
-//header("Location: http://cs.wheaton.edu/~kayley.lane/index.php");
-
-//connect
-$mysqli=mysqli_connect("csdb.wheaton.edu","kayley_lane", "82606", "Csci371FruitRoad");
-if (mysqli_connect_error())
-    die("Failed to connect - " . mysqli_connect_error());
-
+include_once "head.php";
 ?>
 
-
-<head>
-<title> blonic original character do not steal </title>
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
-<link rel="shortcut icon" href="http://i1144.photobucket.com/albums/o488/incarce
-rempb/Icons/MonsterGirls/tinycyclops.png">
+<title> Buy me! </title>
+<link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
+<link rel="shortcut icon" href="http://cdn2.bigcommerce.com/n-biq04i/8f8spg/products/124/images/445/Banana__64657.1405319161.125.100.jpg?c=2">
 <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?fam
 ily=Oswald">
-    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='st
+<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='st
 ylesheet' type='text/css'>
 
 <style type="text/css">
@@ -44,29 +27,26 @@ a:hover { color: #2c2c2c; text-decoration:none}
 a:active { color: #000000; text-decoration: none}
 
 body{
-background-color:#ffffff;
-font-family: verdana;
-color:#2f2f2f;
-font-size: 11px;
+    background-color:#ffffff;
+    font-family: verdana;
+    color:#2f2f2f;
+    font-size: 11px;
 }
 
-
 #contain2{
-text-align: center; 
-margin-top:50px;
-font-size:10px;
-color:#3d3d3d;
-padding-top:10px;
-padding-left:10px;
-padding-right:10px;
-padding-bottom:10px;
-background-color: transparent;
+    text-align: center; 
+    margin-top:50px;
+    font-size:10px;
+    color:#3d3d3d;
+    padding-top:10px;
+    padding-left:10px;
+    padding-right:10px;
+    padding-bottom:10px;
+    background-color: transparent;
 }
     
 #contain2 img{
-        
-      max-width:  500px;
-        
+    max-width:  500px;    
 }
 
 #contain2 h2{
@@ -98,14 +78,6 @@ background-color: transparent;
 #underneath a:hover{
     font-style:italic;
 }
-
-
-.section{
-    display:inline-block;
-    width:710px; padding:5px 5px 5px 5px; background:transparent;
-    height:700px; overflow-x:auto; overflow-y:auto;
-}
-
 h1{
     font-size:14px; font-style:italic; font-family:georgia;
     padding-top:2px; padding-bottom:2px;
@@ -113,7 +85,6 @@ h1{
     color:#cccccc;
     display: inline;
 }
-
 h3{
     font-family:oswald;
     text-align:center;
@@ -121,64 +92,12 @@ h3{
     color:#000000;
 }
 
-    
 #longDescription{
    max-height: 400px; width: 300px; overflow: auto;
    font-size: 11px; color: #757575;
 }
-
-.divider{
-        width: 300px;
-        margin-top: 5px;
-        margin-bottom: 5px;
-        border-bottom-width: 1px;
-        border-bottom-style: solid;
-        border-color: #cccccc;
-}
-    
- #navigation{
-    font-size:16px;
-    width: 100%;
-    height: 25px;
-    padding-top: 5px;
-    text-align: center;
-    background-color: #000000;
-    -webkit-border-bottom-right-radius: 50px;
--webkit-border-bottom-left-radius: 50px;
--moz-border-radius-bottomright: 50px;
--moz-border-radius-bottomleft: 50px;
-border-bottom-right-radius: 50px;
-border-bottom-left-radius: 50px;     
-}
-    
-#navigation a{
-        margin-left: 35px;
-        color: #cccccc;
-        font-family: Verdana;
-        font-style: italic;
-    }
-    
-#navigation a:hover{
-        color: #ffffff;
-    }
-
-
 </style></head>
-
 <body>
-
-    <div id="navigation">
-    
-        <a href="/">home
-        <img src="https://openclipart.org/image/800px/svg_to_png/14720/abadr_Highway.png" height="20px"></a>
-        <a href="/">account
-        <img src="http://png-2.findicons.com/files/icons/1254/flurry_system/128/users.png" height="20px"></a>
-        <a href="/">products
-        <img src="http://pngimg.com/upload/cherry_PNG623.png" height="20px"></a>
-        <a href="/">cart
-        <img src="http://www.robmcintosh.ca/images/shoppingCart.png" height="20px"></a>
-    
-    </div>
 
 <div id="contain2">
 
@@ -210,27 +129,19 @@ echo"<H1>Price:</H1> $" . ($Product->price) . "<div class='divider'></div>";
 echo"<H1>Weight:</H1> " . ($Product->weight) . " kg. <div class='divider'></div>";
 echo "<H1>Product Details:</H1><DIV ID='longDescription'>" . $Product->longDescription . "</DIV> <div class='divider'></div>";
 
-echo "<form id = 'buyer' action='addtocart.php' method='POST' style='float:right'>
-
+//ProductID invisible to post variable to next page?
+echo "  <form id = 'buyer' action='addtocart.php' method='POST' style='float:right'>
         <CENTER>
-        <img src='http://www.inmotionhosting.com/support/images/stories/icons/ecommerce/empty-cart-dark.png' width='50px' align='right'>
-        <label for='addtocart'>Qty:</label>
-        <INPUT type='number' name='quantity' id='addtocart' class ='text ui-widget-content ui-corner-all' style='width:50px;' min='1'  max='9999' value='1' ><br>
-        <button><div style='font-size:10px; color:#666666'>ADD TO CART.</div></button> 
-        <Input type='text' name='productid' value='$productid' style='display:none'>
-        </CENTER>
-
-      </form>";
-
-echo "</TD></TR></TABLE>";
-
+        <Input type='text' name='productid' value='$productid' style='display:none'>";
 ?>
 
-
-
+<img src='http://www.inmotionhosting.com/support/images/stories/icons/ecommerce/empty-cart-dark.png' width='50px' align='right'>
+<label for='addtocart'>Qty:</label>
+<INPUT type='number' name='quantity' id='addtocart' class ='text ui-widget-content ui-corner-all' style='width:50px;' min='1'  max='9999' value='1' ><br>
+<INPUT type='submit' id='addOrder'style='font-size:10px; color:#666666' value='ADD TO CART' onclick="return confirm('Are you sure you want to add this to your cart?');"> 
+</CENTER>
+</form>
+</TD></TR></TABLE>
 </div>
-
-
-
 </body>
 </html>
