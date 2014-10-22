86346356981e6639d16f21b8e40f5ee1ebcb369b
Kayley Lane <kayley.lane@my.wheaton.edu>
2014-10-21 20:57:14 -0500

    Adds a single product to the database.

---
+++ submitaddproduct.php
@@ -0,0 +1,23 @@
+<!-- Add a single product to the database. -->
+<?php
+    include_once "head.php";
+
+    $pname = addslashes($_POST['aname']);
+    $pprice = addslashes($_POST['aprice']);
+    $pweight = addslashes($_POST['aweight']);
+    $picon = addslashes($_POST['aicon']);
+    $pimage = addslashes($_POST['aimage']);
+    $pshort = addslashes($_POST['ashort']);
+    $plong = addslashes($_POST['along']);
+
+    // Insert row
+    // NOTE: PID should be autoincrement, then this pid below is removed.
+    $QryStr = "INSERT INTO Products (pid, name, shortDescription, longDescription, thumbnailUrl, imageUrl, price, weight) 
+    VALUES ('20', '$pname', '$pshort', '$plong', '$picon', '$pimage', '$pprice', '$pweight')";
+
+    mysqli_query($mysqli,$QryStr) or
+        die("Failed query - $QryStr\n" . mysqli_error($mysqli));
+
+    header("Location: editproducts.php");
+
+?>
