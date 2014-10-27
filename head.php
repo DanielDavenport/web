6613570ccd31276b30cd450c6779431f550ab2b8
Kayley Lane <kayley.lane@my.wheaton.edu>
2014-10-27 09:33:40 -0500

    grey again
    (more pages white than orange, can change gradient color in individual pages though)

--- head.php
+++ head.php
@@ -69,11 +69,11 @@ a:active { color: #000000; text-decoration: none}
     height: 75px;
     text-align: center;
     background: #FAB700; /* old browsers */
-    background: -moz-linear-gradient(top, #C79200 0%, #FAB700 100%); /* firefox */
-    border-top: 1px solid #FAB700;
-    border-bottom: 1px solid #FAB700;
-    background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#C79200), color-stop(100%,#FAB700)); /* webkit */
-    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#C79200', endColorstr='#FAB700',GradientType=0 ); /* ie */
+    background: -moz-linear-gradient(top, #CCCCCC 0%, #FFFFFF 100%); /* firefox */
+    border-bottom: 1px solid #ffffff;
+    border-top: 1px solid #C4C4C4;
+    background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#CCCCCC), color-stop(100%,#FFFFFF)); /* webkit */
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#CCCCCC', endColorstr='#FFFFFF',GradientType=0 ); /* ie */
     -webkit-border-radius: 20px;
     -moz-border-radius: 20px;
     border-radius: 20px;
