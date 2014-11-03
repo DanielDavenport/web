306b37448ceee3d884224990b0e6879bbff89005
taylorschuster7 <taylor.schuster@my.wheaton.edu>
2014-11-03 01:34:48 -0600

    Update RegisterUser.php

--- RegisterUser.php
+++ RegisterUser.php
@@ -1,10 +1,10 @@
 <?php
 
 session_start();
+$_SESSION['fullname'] = $_POST['fullname'];
+$_SESSION['email'] = $_POST['email'];
+$_SESSION['address'] = $_POST['address'];
 $_SESSION['username'] = $_POST['username'];
-$_SESSION['username'] = $_POST['username'];
-$_SESSION['username'] = $_POST['username'];
-$_SESSION['username'] = $_POST['username'];
-$_SESSION['username'] = $_POST['username'];
+$_SESSION['password'] = $_POST['password'];
 header("Location: Splash.php");
 ?>
