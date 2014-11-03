43d528cdcff249ea9825ce93d6f4fe8031759d39
taylorschuster7 <taylor.schuster@my.wheaton.edu>
2014-11-03 10:04:47 -0600

    Update RegisterUser.php

--- RegisterUser.php
+++ RegisterUser.php
@@ -1,10 +1,23 @@
 <?php
 
-session_start();
+include_once "head.php";
+
 $_SESSION['fullname'] = $_POST['fullname'];
 $_SESSION['email'] = $_POST['email'];
-$_SESSION['address'] = $_POST['address'];
-$_SESSION['username'] = $_POST['username'];
+$_SESSION['shipAddress'] = $_POST['shipAddress'];
+$_SESSION['billAddress'] = $_POST['billAddress'];
 $_SESSION['password'] = $_POST['password'];
-header("Location: Splash.php");
+
+    if( $pname == "" || ctype_space ($pname) ) 
+        header("Location: editproducts.php");
+    else{
+        // Insert row
+        $QryStr = "INSERT INTO Customer (password, shipAddress, billAddress, email, name) 
+        VALUES ($_SESSION['password'], $_SESSION['shipAddress'], $_SESSION['billAddress'], $_SESSION['email'], $_SESSION['fullname'])";
+        mysqli_query($mysqli,$QryStr) or
+            die("Failed query - $QryStr\n" . mysqli_error($mysqli));
+       header("Location: Splash.php");
+    }
+
+
 ?>
