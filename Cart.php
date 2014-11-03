9e062d48c634c92ace808d0fe86a4a9e2579851f
Josh Wright <joshuawright11@gmail.com>
2014-11-03 10:00:16 -0600

    Cart page added

---
+++ Cart.php
@@ -0,0 +1,19 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<?php include_once "head.php" ?>
+<link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
+<title> An Assortment of Fruits </title>
+<link rel="shortcut icon" href="http://www.juicebeauty.com/store/media/juice-beauty/ingredients/organic-pomegranate.jpg">
+
+ <!-- JQUERY -->
+<script src="http://code.jquery.com/jquery-1.10.2.js" type="text/javascript"></script>
+<script src="http://code.jquery.com/ui/1.10.4/jquery-ui.js" type="text/javascript"></script>
+<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?fam
+ily=Oswald">
+<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='st
+ylesheet' type='text/css'>
+<style type="text/css">
+<body>
+cart ere
+</body>
+</html>
